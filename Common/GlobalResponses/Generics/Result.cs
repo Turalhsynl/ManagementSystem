75374ea15ea4f1ae973263ae40efa9e5d61a4ffb
Husey_vw08 <Husey_vw08@STHQ0123-13>
2025-02-11 16:51:15 +0400

    Add project files.

---
+++ Common/GlobalResponses/Generics/Result.cs
@@ -0,0 +1,16 @@
+﻿namespace Common.GlobalResponses.Generics;
+
+public class Result<T> : Result
+{
+    public T Data { get; set; }
+
+    public Result(List<string> message) : base(message)
+    {
+
+    }
+
+    public Result()
+    {
+        
+    }
+}
---
+++ DAL.SqlServer/Context/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.SqlServer.Context;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Category> Categories { get; set; }
+}
